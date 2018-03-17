76aba180f15e98895f33908d3d22513ff9a89cd3
Marten Stockenberg - Mint VM <mstockenberg@gmail.com>
2018-03-17 13:47:08 +0100

    python PHP File generator startet - currently only class is working

---
+++ FIles/generator.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import errno
+
+
+def class_scaffold():
+    return '''<?php
+            
+namespace {dir};
+
+class {file}{OPEN_BRACKET}
+
+    public function __construct(){OPEN_BRACKET}
+        // Do Something
+    {CLOSE_BRACKET}
+
+{CLOSE_BRACKET}'''
+
+
+def generate_php_files():
+
+    files = tuple(sys.argv[2:])
+    for file in files:
+
+        if not os.path.exists(os.path.dirname(file)):
+            try:
+                os.makedirs(os.path.dirname(file))
+            except OSError as exc:
+                if exc.errno != errno.EEXIST:
+                    raise
+
+        x = open(os.getcwd() + "/" + file + ".php", 'w+')
+        x.write(class_scaffold().format(dir="\\".join(
+                file.split("/")[:-1]),
+                file=file.split("/")[-1],
+                linebreak="\n",
+                OPEN_BRACKET="{",
+                CLOSE_BRACKET="}"))
+
+        x.close()
+
+
+def help_func():
+    print('''
+    Arguments:
+        -php   : Creates a PHP Class
+        -help  : Shows Help
+    ''')
+    pass
+
+
+if len(sys.argv) >= 2:
+    if "-php" in sys.argv[1]:
+        generate_php_files()
+    elif "-help" in sys.argv[1]:
+        help_func()
+else:
+    help_func()
--- Python: Getting Started/Statements and other goodies/exceptions.py
+++ Python: Getting Started/Statements and other goodies/exceptions.py
@@ -1,7 +1,10 @@
 
+import os
+
 student = {"test": "value"}
 
 student['hallo'] = "yuuha"
+x = 1
 
 try:
     hallo = student['hallo']
@@ -13,5 +16,33 @@ except TypeError as e:
     print(e)
 except Exception:
     print('Unknown error')
+except (Exception, TypeError, KeyError):
+    pass
+     # reraise the exception message
 
 print("this happens..")
+
+if x < 0:
+    raise ValueError("Cannot do it")
+
+
+p = '/path'
+
+try:
+    t = ""
+    # process_file(f)
+except OSError as e:
+    print("error in {}".format(e))
+
+
+def make_at(path, dir_name):
+    original_path = os.getcwd()
+    try:
+        os.chdir(path)
+        os.mkdir(dir_name)
+    except OSError as e:
+        print(e)
+        raise
+    finally:
+        os.chdir(original_path)
+
